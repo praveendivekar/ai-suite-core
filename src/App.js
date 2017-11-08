3cd9d439a2515efe3571521f68d73824bc5a30c3
praveendivekar <praveendivekar@gmail.com>
2017-11-08 22:20:17 +0530

    Updated the code with login page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import Login from './Login';
+import FeedData from './FeedData';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">AI Suite</h1>
-        </header>
-        <p className="App-intro">
-          
-        </p>
-      </div>
-    );
-  }
-}
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="feedData" component={FeedData}/>
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
